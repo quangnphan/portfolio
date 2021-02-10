dabd2845b77625d3b57daa5b9502c2277d3ce72c
Quang Phan <qp2194@gmail.com>
2021-02-10 16:34:29 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
   $(window).scroll(function () {
     if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
+      $('.navbar').addClass("sticky");
     } else {
-      $(".navbar").removeClass("sticky");
+      $('.navbar').removeClass("sticky");
     }
   });
   // toggle menu
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
   });
-
-  $('.navbar .menu li a').click(function(){
-    $('html').css("scrollBehavior", "smooth");
-});
+  //typing text animation
+  var typed = new Typed(".typing", {
+    strings: ["Student", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 });
