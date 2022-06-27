fadbc5b460baa64ee6f4cd5613428e08067a2008
Quang Phan <qp2194@gmail.com>
2022-06-26 21:46:26 -0500

    UI: add animation

--- index.js
+++ index.js
@@ -19,12 +19,41 @@ $(document).ready(function () {
 
   //typing text animation
   var typed = new Typed(".typing", {
-    strings: ["Front-end Developer", "CIS Grad"],
+    strings: ["Frontend Developer", "CIS Grad"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
+  //animation
+  AOS.init();
+
   //yearchanged
   $(".copyright").text(new Date().getFullYear());
+
+  //scroll top
+  //Get the button:
+  mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  $(mybutton).click(function()
+	{
+		jQuery('html,body').animate({scrollTop:0},0);
+	})
 });
